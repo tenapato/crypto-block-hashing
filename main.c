46a877703df70a58d888773b8330d4b30bcbd34d
tenapato <tenapatricio@gmail.com>
2021-06-03 14:21:05 -0700

    Ahora se puede ingresar una dificultada y encontrar un cada con n numero de 0's

--- main.c
+++ main.c
@@ -9,20 +9,49 @@
 
 int main(){
     int num = 0;
-    char pass[1000];
-
-    for (int i = 0; i < 1000; i++){
-
+    char *pass = (char *)malloc(sizeof(char)*1000);	
+	char *hash = (char *)malloc(sizeof(char)*1000);
+	int dificultad = 5;
+	char *strToCompare = (char *)malloc(dificultad);
+	char *ceros = (char *)malloc(sizeof(char)*dificultad);
+	//int *aux;
+	//int *fin = ceros + dificultad;
+	
+	for (int i = 0; i < dificultad; i++)
+	{
+		strcat(ceros, "0");
+		
+	}
+	
+	
+	
+    for (int i = 0; i < 1000000000; i++){
+		
         sprintf(pass, "%d", num);
+		hash = crypt(pass, "$5$");
+		hash = hash+4;
+        printf("Num: %d, Hash: %s\n", num, hash);
+		
+		strncpy(strToCompare, hash, dificultad);
+		printf("Str %s Ceros: %s\n", strToCompare, ceros);
 
-        printf("Num: %d, Hash: %s\n", num, crypt(pass, "$5$"));
+		if (!strcmp(strToCompare, ceros))
+		{
+			printf("Es el numero buscado es: %s -------\n", pass);
+			break;
+		}
+		
+
 
         num++;
 
-    }
+    } 
     
+	
 
    
-
+	free(pass);
+	free(ceros);
+	//free(hash);
     return 0;
 }
